d58e488841ca450438721e3ea3f044d82349aa80
perezsolerj <perezsolerj@gmail.com>
2015-02-20 18:37:32 +0100

    Added number of orders measure

--- src/measurer.cpp
+++ src/measurer.cpp
@@ -2,10 +2,12 @@
 #include <geometry_msgs/Pose.h>
 #include <geometry_msgs/WrenchStamped.h>
 #include <tf/transform_broadcaster.h>
+#include <std_msgs/Float64MultiArray.h>
 
 
 #define POSETOPIC "g500/pose"
 #define FORCETOPIC "g500/ForceSensor"
+#define THRUSTERSTOPIC "g500/thrusters_input"
 
 #define WAYPOINT1 (5,15,8)
 #define WAYPOINT2 (15,30,10)
@@ -15,19 +17,21 @@
 
 class Measurer{
   public:
-    ros::Subscriber sub_pose, sub_force;
+    ros::Subscriber sub_pose, sub_force,sub_thrusters;
     double distance;
     double last_position[3];
     ros::Time last_collision, initial_time;
-    int collisions;
+    int collisions, orders;
     int current_waypoint;
     std::vector<tf::Vector3> waypoints;
+    std::vector<float> last_thrusters;
 
     int started;
     
     Measurer();
     void callbackPose(const geometry_msgs::Pose& msg);
     void callbackForce(const geometry_msgs::WrenchStamped& msg);
+    void callbackThrusters(const std_msgs::Float64MultiArray& msg);
 };
 
 void Measurer::callbackPose(const geometry_msgs::Pose& msg){
@@ -36,15 +40,14 @@ void Measurer::callbackPose(const geometry_msgs::Pose& msg){
    distance+=sqrt(pow(last_position[0]-msg.position.x,2)+
                 pow(last_position[1]-msg.position.y,2)+
                 pow(last_position[2]-msg.position.z,2));
- else
-   started=1;
+
  last_position[0]=msg.position.x;
  last_position[1]=msg.position.y;
  last_position[2]=msg.position.z;
 
  if(current_waypoint< waypoints.size() and waypoints[current_waypoint].distance(tf::Vector3(msg.position.x,msg.position.y,msg.position.z))<2.0){
    std::cout<<"WAYPOINT PASADO"<<std::endl;
-   std::cout<<"Distance: "<<distance<<" Ncollisions: "<<collisions<<" Time: "<<(ros::Time::now()-initial_time).toSec()<<std::endl;
+   std::cout<<"Distance: "<<distance<<" Ncollisions: "<<collisions<<" Time: "<<(ros::Time::now()-initial_time).toSec()<<" Orders: "<<orders<<std::endl;
    current_waypoint++;
  }
 
@@ -54,7 +57,7 @@ void Measurer::callbackPose(const geometry_msgs::Pose& msg){
 void Measurer::callbackForce(const geometry_msgs::WrenchStamped& msg){
 
 
-  if ((ros::Time::now()-last_collision).toSec()>=0.5){
+  if (started and (ros::Time::now()-last_collision).toSec()>=0.5){
     if(msg.wrench.force.x !=0  or msg.wrench.force.y != 0 or msg.wrench.force.z != 0 or
         msg.wrench.torque.x != 0 or msg.wrench.torque.y != 0 or msg.wrench.torque.z != 0){
       collisions++;
@@ -65,6 +68,32 @@ void Measurer::callbackForce(const geometry_msgs::WrenchStamped& msg){
   //std::cout<<collisions<<"  "<<msg.wrench.force.x<<"  "<<msg.wrench.force.y<<"  "<<msg.wrench.force.z<<"  "<<msg.wrench.torque.x<<"  "<<msg.wrench.torque.y<<"  "<<msg.wrench.torque.z<<std::endl;
 }
 
+void Measurer::callbackThrusters(const std_msgs::Float64MultiArray& msg){
+  int neworder=0;
+
+  if(started){
+    for(int i=0; i< msg.data.size();i++){
+      if(msg.data[i]*last_thrusters[i]<=0 and (msg.data[i]!=0 or last_thrusters[i]!=0))
+	neworder++;
+    }
+    if(neworder)
+      orders++;
+  }
+  else
+     for(int i=0; i< msg.data.size();i++){
+      if(msg.data[i]!=0){
+	started=1;
+        std::cout<<"Started measuring"<<std::endl;
+      }
+    } 
+  last_thrusters.clear();
+  for(int i=0; i< msg.data.size();i++){
+    last_thrusters.push_back(msg.data[i]);
+  }
+
+  //std::cout<<"orders: "<<orders<<std::endl;
+}
+
 Measurer::Measurer(){
   ros::NodeHandle nh;
   started=0;
@@ -72,9 +101,14 @@ Measurer::Measurer(){
   last_collision=ros::Time::now();
   initial_time=ros::Time::now();
   collisions=0;
+  orders=0;
+  last_position[0]=0;
+  last_position[1]=0;
+  last_position[2]=0;
 
   sub_pose = nh.subscribe(POSETOPIC, 1000, &Measurer::callbackPose,this);
   sub_force = nh.subscribe(FORCETOPIC, 1000, &Measurer::callbackForce,this);
+  sub_thrusters = nh.subscribe(THRUSTERSTOPIC, 1000, &Measurer::callbackThrusters,this);
 
   waypoints.push_back(tf::Vector3 WAYPOINT1);
   waypoints.push_back(tf::Vector3 WAYPOINT2);
